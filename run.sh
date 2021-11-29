06d95fbe933770d33cc539bb2e0b3692c6e6a456
vaderrama <juan.alvarezz.11@gmail.com>
2021-11-29 20:45:18 +0100

    commit test

--- run.sh
+++ run.sh
@@ -6,11 +6,10 @@ sudo apt-get update
 # instalamos pip
 sudo apt install python3-pip
 
-#instalamos flask
-sudo apt install python3-flask
-
 #instalamos
 pip3 install -U -r requirements.txt
 
 # run
+cd app
+
 flask run
